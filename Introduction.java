1bb8e1ace2bff297abe77c626e034028fcbe612f
Ian Thompson <ianthompson@nicelion.com>
2017-08-25 09:55:41 -0400

    Initial sharing of project

---
+++ Introduction.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class Introduction here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Introduction
+{
+    // instance variables - replace the example below with your own
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Ian Thompson");
+        System.out.println("I was born in Spartanburg, SC");
+        System.out.println("I would like to go to Clemson");
+        System.out.println("I like to fish and I work at Bojangles");
+        
+        int year = 2017;
+        int age = 16;
+        
+        System.out.println("year - age");
+        System.out.println(year - age);
+        System.out.println("I am " + age + " years old.");
+        
+    }
+    
+    
+}
